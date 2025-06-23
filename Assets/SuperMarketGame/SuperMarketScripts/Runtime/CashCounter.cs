b394a2fecc87359d1b428368ac2f57038f913453
Mohsin-NS <mohsinfarooq@gamedistrict.co>
2025-06-23 15:16:58 +0500

    chas counter ready

---
+++ Assets/SuperMarketGame/SuperMarketScripts/Runtime/CashCounter.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CashCounter : MonoBehaviour
+{
+    [Header("UI Elements")]
+    [SerializeField] private Text amountDueText;    
+    [SerializeField] private Text enteredAmountText; 
+
+    [Header("Settings")]
+    [SerializeField] private float amountDue = 100.00f; 
+
+    private string enteredAmount = ""; 
+
+    private void Start()
+    {
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        amountDueText.text = $"Amount Due: ${amountDue:F2}";
+        enteredAmountText.text = $"Entered: ${enteredAmount}";
+    }
+
+    public void OnKeyPress(string key)
+    {
+        if (key == "." && enteredAmount.Contains(".")) return;
+ 
+        enteredAmount += key;
+        UpdateDisplay();
+    }
+
+    public void OnBackspacePress()
+    {
+        if (enteredAmount.Length > 0)
+        {
+            enteredAmount = enteredAmount.Substring(0, enteredAmount.Length - 1);
+            UpdateDisplay();
+        }
+    }
+
+    public void OnConfirmPress()
+    {
+        if (float.TryParse(enteredAmount, out float enteredValue) && Mathf.Approximately(enteredValue, amountDue))
+        {
+            Debug.Log("Payment Successful!");
+            PaymentSuccessful();
+        }
+        else
+        {
+            Debug.Log("Incorrect Amount! Please try again.");
+        }
+    }
+
+    private void PaymentSuccessful()
+    {
+        enteredAmount = "";
+        UpdateDisplay();
+    }
+}
+
